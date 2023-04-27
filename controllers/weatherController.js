28604532a607edae3c334b82a2173cfce833045d
TanikaS <tanika21000@student.hamk.fi>
2023-04-27 13:37:01 +0300

    updates

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -6,16 +6,17 @@ const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
 async function getWeatherData(req, res) {
   const location = req.query.location;
 
+
+  //using only finnish postal codes
   let queryUrl;
-  if (isNaN(location)) {
-    queryUrl = `${baseUrl}?q=${location}&appid=${apiKey}&units=metric`;
-  } else if (location.length === 5) {
-    queryUrl = `${baseUrl}?zip=${location}&appid=${apiKey}&units=metric`;
-  } else if (location.includes(',')) {
+  if (/^\d{5}(?:\d{2})?$/.test(location)) {
+    queryUrl = `${baseUrl}?zip=${location},fi&appid=${apiKey}&units=metric`;
+  } else if (/^(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?)$/.test(location)) {
+    
     const [lat, lon] = location.split(',');
     queryUrl = `${baseUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   } else {
-    return res.render('weather', { error: 'Invalid location' });
+    queryUrl = `${baseUrl}?q=${location}&appid=${apiKey}&units=metric`;
   }
 
   try {
@@ -25,7 +26,10 @@ async function getWeatherData(req, res) {
       const temp = data.main.temp;
       const city = data.name;
       const country = data.sys.country;
-      res.render('weather', { temp, city, country });
+      const humidity = data.main.humidity;
+      const windSpeed = data.wind.speed;
+      const weatherDescription = data.weather[0].description;
+      res.render('weather', { temp, city, country, humidity, windSpeed, weatherDescription });
     } else {
       res.render('weather', { error: `Error ${response.status}: ${response.statusText}` });
     }
