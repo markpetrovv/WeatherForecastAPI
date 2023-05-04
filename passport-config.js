3c2f13d3753dc853836e73d6249ca4c6f14ab440
Mark Petrov <petrovv.mark00@gmail.com>
2023-05-05 00:35:55 +0300

    small changes for code readability

--- passport-config.js
+++ passport-config.js
@@ -5,10 +5,10 @@ module.exports = function(passport) {
   passport.use(new LocalStrategy((username, password, done) => {
     User.findOne({ username: username }, (err, user) => {
       if (err) return done(err);
-      if (!user) return done(null, false, { message: 'Incorrect username.' });
+      if (!user) return done(null, false, {message: 'Incorrect username.'});
 
       if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
+        return done(null, false, {message: 'Incorrect password.'});
       }
 
       return done(null, user);
